7eea5868bb943da822a06193032d8416e54d281a
Donatas Noreika <donoras@gmail.com>
2022-01-27 09:36:26 +0200

    Pridėjau priešo tanko generavimą

--- tankas.py
+++ tankas.py
@@ -1,3 +1,4 @@
+from random import randint
 
 class Tankas:
     def __init__(self):
@@ -5,10 +6,18 @@ class Tankas:
         self._y = 0
         self._kryptis = "Š"
         self._suviai = [0, 0, 0, 0]
+        self._prieso_x = 0
+        self._prieso_y = 0
+        self._generuoti_priesa()
 
     def info(self):
         print(f"Koordinatės: x: {self._x}, y: {self._y}, kryptis: {self._kryptis}")
         print(f"Šūviai: {sum(self._suviai)}, kryptys: {self._suviai}")
+        print(f"Priešo koordinatės: x: {self._prieso_x}, y: {self._prieso_y}")
+
+    def _generuoti_priesa(self):
+        self._prieso_x = randint(-10, 10)
+        self._prieso_y = randint(-10, 10)
 
     def siaure(self):
         self._y += 1
